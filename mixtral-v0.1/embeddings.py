af43596a78b59f7dfe3844ac8c23af8f84982009
Gene Louis Kim <genekim@usf.edu>
2024-03-15 17:13:52 -0400

    Updated mixtral's embeddings.py

--- mixtral-v0.1/embeddings.py
+++ mixtral-v0.1/embeddings.py
@@ -1,19 +1,11 @@
-from langchain.embeddings import LlamaCppEmbeddings
-from langchain import PromptTemplate
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_community.embeddings import LlamaCppEmbeddings
 
 # Make sure the model path is correct for your system!
 llm = LlamaCppEmbeddings(
     model_path="/home/gene/research/llms/gguf_llms/models/mixtral-8x7b-v0.1.Q4_K_M.gguf",
     verbose=True,
-    n_ctx=2048,
-    f16_kv=True, 
+    n_gpu_layers=28, # How many layers to offload to gpu?
 )
-
-prompt = """
-Question: A rap battle between Stephen Colbert and John Oliver
-"""
 
 # Example texts.
 texts = [
